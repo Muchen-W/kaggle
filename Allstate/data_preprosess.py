97dcd3e030efbea28a1125b9145b6756b3ccf30d
matthew <muchen.w@gmail.com>
2016-11-16 00:27:56 -0800

    replace letter categories with 0/1 matrix

--- Allstate/data_preprosess.py
+++ Allstate/data_preprosess.py
@@ -1,10 +1,29 @@
 import pandas as pd
 
-data_tr = pd.read_csv('train.csv')
+def data_preprocess(fread, fwrite):
+	df_tr = pd.read_csv(fread)
+	for key in df_tr.keys():
+		if 'cat' in key:
+			for item in df_tr[key].unique():
+				df_tr[key+'_'+item] = df_tr[key].apply(lambda x: 1 if x == item else 0)
+			df_tr.drop(key, axis=1, inplace=True)
+	
+	#key_drop = ['cat'+str(i+1) for i in range(116)]
+	#df_tr.drop(key_drop, axis=1, inplace=True)
+	df_tr.to_csv(fwrite, index=False)
+	return df_tr
 
-for key in data_tr.keys():
-	if 'cat' in key:
-		for item in data_tr[key].unique():
-			data_tr[key+'_'+item] = 0
-		data_tr.drop(key, axis=1, inplace=True)
+def sample_trial():
+	fread = 'train_sample.csv'
+	fwrite = 'train_sample_preprocessed.csv'
+	df_tr = data_preprocess(fread, fwrite)
+	print(df_tr.describe())
 
+def complete_train_data():
+	fread = 'train.csv'
+	fwrite = 'train_preprocessed.csv'
+	df_tr = data_preprocess(fread, fwrite)
+
+if __name__ == '__main__':
+	sample_trial()
+	#complete_train_data()
